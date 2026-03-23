da80f3094b4b2488b02412c15ea5a751477018fc
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2026-03-24 00:35:05 +0530

    add manual binding

--- manual.js
+++ manual.js
@@ -1,2 +1,10 @@
-const sharedInput = document.getElementById("sharedInput");
-const manualOutput = document.getElementById("manualOutput");
+console.log("Hello")
+const sharedInput = document.getElementById("sharedInput")
+const manualOutput = document.getElementById("manualOutput")
+
+
+sharedInput.addEventListener("input", () => {
+  manualOutput.textContent = sharedInput.value
+})
+
+console.log("In")
