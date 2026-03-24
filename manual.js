7323e4568b6ce65334596c04d1d33b65066b1607
Jithin Aji <jithinaji@Jithins-MacBook-Air.local>
2026-03-25 00:05:58 +0530

    add Object.define update

--- manual.js
+++ manual.js
@@ -1,10 +1,33 @@
-console.log("Hello")
+const state = {
+  value: ""
+}
+
 const sharedInput = document.getElementById("sharedInput")
 const manualOutput = document.getElementById("manualOutput")
+const defineOutput = document.getElementById("defineOutput")
+const proxyOutput = document.getElementById("proxyOutput")
 
+const renderManual = () => {
+  manualOutput.textContent = state.value
+}
+
+let internalValue = ""
+const renderDefine = () => {
+  Object.defineProperty(state, "value", {
+    set(newvalue) {
+      internalValue = newvalue
+      defineOutput.textContent = newvalue
+    },
+    get() {
+      return internalValue
+    }
+  })
+}
+
+renderDefine()
 
 sharedInput.addEventListener("input", () => {
-  manualOutput.textContent = sharedInput.value
+  state.value = sharedInput.value
+  renderManual()
 })
 
-console.log("In")
